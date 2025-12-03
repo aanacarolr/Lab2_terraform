d5a5ceff8cd7f2fe734686a8289348a393c307ca
Your Name <you@example.com>
2025-12-03 20:00:39 +0100

    Add variables

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs in two AZs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs in two AZs"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for web servers"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 key pair name for SSH access"
+  type        = string
+  default     = ""
+}
+
+variable "project_name" {
+  description = "Tag value for project name"
+  type        = string
+  default     = "terraform-ha-webapp"
+}
+x
